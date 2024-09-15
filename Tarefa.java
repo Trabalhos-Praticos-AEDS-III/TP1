d7529e529f25fbe7b711cd49c3ac5a7140a4340d
Luis Toledo <augustootledo.luis@gmail.com>
2024-09-15 17:55:16 -0300

    TP01 CRUD completo, favor realizar Testes

--- Tarefa.java
+++ Tarefa.java
@@ -9,24 +9,27 @@ import java.time.LocalDate;
 
 public class Tarefa implements Registro
 {
-    private int id;
-    private String nome;
-    private LocalDate data_criacao;
-    private LocalDate data_conclusao;
-    private String status;
-    private short prioridade;
+    private int id;                     // Identificador unico da tarefa
+    private String nome;                // Nome ou descricao da tarefa
+    private LocalDate data_criacao;     // Data em que a tarefa foi criada
+    private LocalDate data_conclusao;   // Data em que a tarefa foi concluída
+    private String status;              // Status atual da tarefa ("Pendente", "Concluída", "Em Progresso")
+    private byte prioridade;            // Nivel de prioridade da tarefa (0-5)
 
+    // Construtor padrao, inicializa com valores padrão
     public Tarefa()
     {
-        this(-1, "", LocalDate.now(), LocalDate.now(), "", (short) 0);
+        this(-1, "", LocalDate.now(), LocalDate.now(), "", (byte) 0);
     }
 
-    public Tarefa(String n, LocalDate x, LocalDate y, String s, short p)
+    // Construtor para escrever no arquivo, onde ainda nao temos o ID correto
+    public Tarefa(String n, LocalDate x, LocalDate y, String s, byte p)
     {
         this(-1, n, x, y, s, p);
     }
 
-    public Tarefa(int i, String n, LocalDate x, LocalDate y, String s, short p)
+    // Construtor completo para inicializacao com todos os campos
+    public Tarefa(int i, String n, LocalDate x, LocalDate y, String s, byte p)
     {
         this.id = i;
         this.nome = n;
@@ -36,31 +39,42 @@ public class Tarefa implements Registro
         this.prioridade = p;
     }
 
-    public void setId(int id)
-    {
-        this.id = id;
-    }
+    // Metodos Getters e Setters
+    public int getId() { return this.id; }
+    public void setId(int id) { this.id = id; }
 
-    public int getId()
-    {
-        return this.id;
-    }
+    public String getNome() { return this.nome; }
+    public void setNome(String nome) { this.nome = nome; }
 
+    public LocalDate getDataCriacao() { return this.data_criacao; }
+    public void setDataCriacao(LocalDate data_criacao) { this.data_criacao = data_criacao; }
+
+    public LocalDate getDataConclusao() { return this.data_conclusao; }
+    public void setDataConclusao(LocalDate data_conclusao) { this.data_conclusao = data_conclusao; }
+
+    public String getStatus() { return this.status; }
+    public void setStatus(String status) { this.status = status; }
+
+    public byte getPrioridade() { return this.prioridade; }
+    public void setPrioridade(byte prioridade) { this.prioridade = prioridade; }
+
+    // Converte todos os campos da instancia em um array de bytes
     public byte[] toByteArray() throws IOException
     {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         DataOutputStream dos = new DataOutputStream(baos);
 
-        dos.write(this.id);
+        dos.writeInt(this.id);
         dos.writeUTF(this.nome);
         dos.writeInt((int) this.data_criacao.toEpochDay());
         dos.writeInt((int) this.data_conclusao.toEpochDay());
         dos.writeUTF(this.status);
-        dos.writeShort(this.prioridade);
+        dos.writeByte(this.prioridade);
 
         return baos.toByteArray();
     }
 
+    // Converte um array de bytes em valores e os atribui aos campos da instancia
     public void fromByteArray(byte[] b) throws IOException
     {
         ByteArrayInputStream bais = new ByteArrayInputStream(b);
@@ -71,7 +85,17 @@ public class Tarefa implements Registro
         this.data_criacao = LocalDate.ofEpochDay(dis.readInt());
         this.data_conclusao = LocalDate.ofEpochDay(dis.readInt());
         this.status = dis.readUTF();
-        this.prioridade = dis.readShort();
+        this.prioridade = dis.readByte();
+    }
+
+    // Retorna uma representacao textual da instancia da tarefa
+    public String toString() {
+        return "\nID............: " + this.id +
+               "\nNome..........: " + this.nome +
+               "\nData Criação..: " + this.data_criacao +
+               "\nData Conclusão: " + this.data_conclusao +
+               "\nStatus........: " + this.status +
+               "\nPrioridade....: " + this.prioridade;
     }
 
 }
--- Utils/Arquivo.java
+++ Utils/Arquivo.java
@@ -6,50 +6,62 @@ import java.lang.reflect.Constructor;
 
 public class Arquivo<T extends Registro>
 {
-    final int TAM_CABECALHO = 4;
-    RandomAccessFile arquivo;
-    String nome_arquivo;
-    Constructor<T> construtor;
-    HashExtensivel<ParIDEndereco> indice_direto;
+    final int TAM_CABECALHO = 4;        // Tamanho do cabeçalho do arquivo (4 bytes para guardar o ID para proximo registro)
+    RandomAccessFile arquivo;           // Manipulador para o arquivo de dados
+    String nome_arquivo;                // Nome do arquivo de dados
+    Constructor<T> construtor;          // Construtor da classe T
+    HashExtensivel<ParIDEndereco> indice_direto; // indice direto para acesso rapido aos registros
 
+    // Construtor da classe Arquivo (na = Nome para o arquivo)
     public Arquivo(String na, Constructor<T> c) throws Exception
     {
-        File d = new File(".\\dados" + na + ".db");
+        // Cria o diretorio de dados se nao existir
+        File d = new File(".\\dados");
         if(!d.exists()) d.mkdir();
 
+        // Define o nome do arquivo e abre o RandomAccessFile para leitura e escrita
         this.nome_arquivo = ".\\dados\\" + na + ".db";
         this.construtor = c;
         arquivo = new RandomAccessFile(this.nome_arquivo, "rw");
+
+        // Inicializa o cabecalho do arquivo se ele nao existir (Setar ID como 0)
         if (arquivo.length() < TAM_CABECALHO) arquivo.writeInt(0);
 
+        // Inicializa o indice direto com os arquivos de indice
         indice_direto = new HashExtensivel<>(
             ParIDEndereco.class.getConstructor(), 
             4, 
-            ".\\dados\\"+na+".hash_d.db", 
-            ".\\dados\\"+na+".hash_c.db"
+            ".\\dados\\" + na + ".hash_d.db", 
+            ".\\dados\\" + na + ".hash_c.db"
         );
     }
 
+    // Cria um novo registro no arquivo
     public int create(T obj) throws Exception
     {
+        // Atualiza o ID do próximo registro
         arquivo.seek(0);
         int proximo_id = arquivo.readInt() + 1;
         arquivo.seek(0);
         arquivo.writeInt(proximo_id);
         obj.setId(proximo_id);
+
+        // Move o ponteiro para o final do arquivo e escreve o novo registro
         arquivo.seek(arquivo.length());
         long endereco = arquivo.getFilePointer();
 
         byte[] b = obj.toByteArray();
-        arquivo.writeByte(' ');
-        arquivo.writeShort(b.length);
-        arquivo.write(b);
+        arquivo.writeByte(' ');       // Marca o registro como válido
+        arquivo.writeShort(b.length);   // Escreve o tamanho do registro
+        arquivo.write(b);               // Escreve o conteúdo do registro
 
+        // Adiciona a entrada no indice direto
         indice_direto.create(new ParIDEndereco(proximo_id, endereco));
 
         return obj.getId();
     }
 
+    // Le um registro do arquivo baseado no ID
     public T read(int id) throws Exception
     {
         T obj;
@@ -57,25 +69,29 @@ public class Arquivo<T extends Registro>
         byte[] array_registro;
         byte lapide;
 
+        // Recupera o endereco do registro a partir do indice direto
         ParIDEndereco pid = indice_direto.read(id);
         if(pid != null)
         {
+            // Move o cursor do arquivo para onde deve comecar a ler 
             arquivo.seek(pid.getEndereco());
             obj = construtor.newInstance();
             lapide = arquivo.readByte();
+            // Se tiver marcado como valido, ler sequencialmente a quantidade de bytes que esta marcado como tamanho do registro
             if (lapide == ' ')
             {
                 tamanho = arquivo.readShort();
                 array_registro = new byte[tamanho];
                 arquivo.read(array_registro);
                 obj.fromByteArray(array_registro);
-                if (obj.getId() != id) return obj;
+                if (obj.getId() == id) return obj; // Verifica se o ID corresponde
             }
         }
 
-        return null;
+        return null; // Retorna null se o registro nao for encontrado
     }
 
+    // Remove um registro do arquivo baseado no ID
     public boolean delete(int id) throws Exception
     {
         T obj;
@@ -83,19 +99,23 @@ public class Arquivo<T extends Registro>
         byte[] array_registro;
         byte lapide;
 
+        // Recupera o endereço do registro a partir do indice direto
         ParIDEndereco pie = indice_direto.read(id);
         if (pie != null)
         {
             arquivo.seek(pie.getEndereco());
             obj = construtor.newInstance();
             lapide = arquivo.readByte();
-            if(lapide==' ') {
+            // Se tiver marcado como valido, ler sequencialmente a quantidade de bytes que esta marcado como tamanho do registro
+            if(lapide == ' ') 
+            {
                 tamanho = arquivo.readShort();
                 array_registro = new byte[tamanho];
                 arquivo.read(array_registro);
                 obj.fromByteArray(array_registro);
-                if(obj.getId()==id)
+                if(obj.getId() == id)
                 {
+                    // Marca o registro como excluído
                     if(indice_direto.delete(id))
                     {
                         arquivo.seek(pie.getEndereco());
@@ -106,9 +126,10 @@ public class Arquivo<T extends Registro>
             }
         }
 
-        return false;
+        return false; // Retorna false se o registro nao for encontrado ou nao puder ser excluido
     }
 
+    // Atualiza um registro existente com novos dados
     public boolean update(T novo_obj) throws Exception
     {
         T obj;
@@ -116,12 +137,14 @@ public class Arquivo<T extends Registro>
         byte[] array_registro;
         byte lapide;
 
+        // Recupera o endereço do registro a partir do indice direto
         ParIDEndereco pie = indice_direto.read(novo_obj.getId());
         if (pie != null)
         {
             arquivo.seek(pie.getEndereco());
             obj = construtor.newInstance();
             lapide = arquivo.readByte();
+            // Se tiver marcado como valido, ler sequencialmente a quantidade de bytes que esta marcado como tamanho do registro
             if (lapide == ' ')
             {
                 tamanho = arquivo.readShort();
@@ -133,25 +156,24 @@ public class Arquivo<T extends Registro>
                     byte[] novo_array_registro = novo_obj.toByteArray();
                     short novo_tamanho = (short) novo_array_registro.length;
 
-                    // Sobrescrever o registro
+                    // Atualiza o registro se o novo tamanho for menor ou igual ao tamanho antigo
                     if (novo_tamanho <= tamanho)
                     {
-                        // Por que pular o tamanho ?
                         arquivo.seek(pie.getEndereco() + 3);
                         arquivo.write(novo_array_registro);
                     }
-                    // Move o novo registro para o fim
+                    // Move o novo registro para o fim do arquivo se o novo tamanho for maior
                     else
                     {
                         arquivo.seek(pie.getEndereco());
-                        arquivo.write('*');
-                        arquivo.seek(arquivo.length());
+                        arquivo.write('*');             // Marcar registro antigo como excluido
+                        arquivo.seek(arquivo.length());   // Ir para o final para escrever o registro atualizado
                         long novo_endereco = arquivo.getFilePointer();
 
-                        arquivo.writeByte(' ');
-                        arquivo.writeShort(novo_tamanho);
-                        arquivo.write(novo_array_registro);
-                        indice_direto.update(new ParIDEndereco(novo_obj.getId(), novo_endereco));
+                        arquivo.writeByte(' ');             // Marca o registro como válido
+                        arquivo.writeShort(novo_tamanho);     // Escreve o tamanho do registro
+                        arquivo.write(novo_array_registro);   // Escreve o conteúdo do registro
+                        indice_direto.update(new ParIDEndereco(novo_obj.getId(), novo_endereco)); // Atualiza o indice na hash
                     }
 
                     return true;
@@ -159,13 +181,13 @@ public class Arquivo<T extends Registro>
             }
         }
 
-        return false;
+        return false; // Retorna false se o registro não for encontrado ou não puder ser atualizado
     }
 
+    // Fecha o arquivo e o indice direto
     public void close() throws Exception
     {
         arquivo.close();
         indice_direto.close();
     }
-
 }
